b6b4a4922acb287834f6c3a57bc1985c2aedcc89
Bálint Fodor <balint.fodor@gmail.com>
2018-05-16 13:30:01 +0200

    added scripts

---
+++ src/app/scripts/a3dc core/module_autoThresholdOtsu.py
@@ -0,0 +1,17 @@
+import a3dc
+from a3dc import Arg
+import numpy as np
+from scripts.imageProcessing import autoThreshold
+from scripts.a3dc_utils import multi_dim_image_to_numpy_array, multi_dim_image_plane_iterator
+
+def module_main():
+    im = multi_dim_image_to_numpy_array(a3dc.inputs['in'])
+    im = autoThreshold(im, 'Otsu')
+    a3dc.outputs['out'] = a3dc.MultiDimImageFloat(a3dc.inputs['in'].dims())
+    for i, plane in enumerate(multi_dim_image_plane_iterator(a3dc.outputs['out'])):
+        np.copyto(plane, im[i])
+
+inputs = [Arg('in', a3dc.types.ImageFloat)]
+outputs = [Arg('out', a3dc.types.ImageFloat)]
+
+a3dc.def_process_module(inputs, outputs, module_main)
---
+++ src/app/scripts/a3dc_utils.py
@@ -0,0 +1,35 @@
+import a3dc
+import numpy as np
+
+
+def multi_dim_image_to_numpy_array(image):
+    """
+    Returns a numpy array with the copied data of the a3dc.MultiDimImage
+    argument 'image'.
+    """
+    out = np.zeros(image.dims())
+    for i, plane in enumerate(multi_dim_image_plane_iterator(image)):
+        np.copyto(out[i], plane)
+
+    return out
+
+
+def multi_dim_image_plane_iterator(image):
+    """
+    Returns a generator object can iterate through the planes of the 'image'.
+    """
+    for i in range(image.dims()[2]):
+        non_copied_slice = np.array(image.plane([i]), copy=False)
+        yield non_copied_slice
+
+
+def multi_dim_image_apply(image, plane_func):
+    """
+    Applies 'plane_func' to all the planes of the 'image', treating one plane
+    at a time. 'plane_func' should be a function with the signature
+    plane_func(plane) -> None and should manupulate the given plane in-place.
+    No copy of the 'image' data is made. This function has no return object,
+    manipulates 'image' in-place.
+    """
+    for plane in multi_dim_image_plane_iterator(image):
+        plane_func(plane)
