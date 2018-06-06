e6fe286c846f7e3dddc0394428de33ae612973af
Bálint Fodor <balint.fodor@gmail.com>
2018-06-06 14:28:51 +0200

    wrapped py thresholding works

--- src/app/modules/a3dc_modules/proto_threshold.py
+++ src/app/modules/a3dc_modules/proto_threshold.py
@@ -1,13 +1,19 @@
 import a3dc_module_interface as a3
 from modules.a3dc_modules.a3dc.interface import threshold
+from modules.a3dc_modules.a3dc.imageclass import Image
+import numpy as np
 
 
 def make_threshold_module(method):
 
     def proto_threshold_main():
         im = a3.MultiDimImageFloat_to_ndarray(a3.inputs['input'])
-        im = threshold(image=im, method=method)
-        a3.outputs['output'] = a3.MultiDimImageFloat_from_ndarray(im)
+        a3dc_image = Image(im, {'Name': 'random_name', 'Type': np.float})
+
+        a3dc_out, log_text = threshold(image=a3dc_image, method=method)
+        print(log_text)
+
+        a3.outputs['output'] = a3.MultiDimImageFloat_from_ndarray(a3dc_out.array)
 
     inputs = [a3.Arg('input', a3.types.ImageFloat)]
     outputs = [a3.Arg('output', a3.types.ImageFloat)]
