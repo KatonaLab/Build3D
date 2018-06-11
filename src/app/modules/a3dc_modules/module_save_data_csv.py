fd0cc7fe612d6b94e7882d74235d618c1ae6e2b5
Bálint Fodor <balint.fodor@gmail.com>
2018-06-11 12:13:50 +0200

    save data/image wrappers, to be tested

---
+++ src/app/modules/a3dc_modules/module_save_data_csv.py
@@ -0,0 +1,19 @@
+import a3dc_module_interface as a3
+from modules.a3dc_modules.a3dc.interface import save_data
+from modules.a3dc_modules.a3dc.imageclass import Image
+import numpy as np
+
+def module_main():
+    tagged_im = a3.MultiDimImageUInt32_to_ndarray(a3.inputs['tagged input'])
+    db = a3.inputs['db output']
+    a3dc_tagged_image = Image(tagged_im, metadata={'Name': 'random_name', 'Type': np.uint32}, database=db)
+
+    log_text = save_data([a3dc_tagged_image], './', toText=True)
+    print(log_text)
+
+
+inputs = [a3.Arg('tagged input', a3.types.ImageUInt32), a3.Arg('db', a3.types.GeneralPyType)]
+
+outputs = []
+
+a3.def_process_module(inputs, outputs, module_main)
