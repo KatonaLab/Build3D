b4c5d32165970d0a76df5b225a4688012e0d059b
Balint Fodor <balint.fodor@gmail.com>
2018-08-29 14:41:04 +0200

    adjusting .bat file to the modified program arguments

--- a3-dc.bat
+++ a3-dc.bat
@@ -1,2 +1,2 @@
 set PATH=python-3.6.5.amd64;%PATH%
-start "" a3-dc.exe --python-path "python-3.6.5.amd64" --module-path "modules" --editor
+start "" a3-dc.exe --python-home "python-3.6.5.amd64" --module-path "modules" --editor
